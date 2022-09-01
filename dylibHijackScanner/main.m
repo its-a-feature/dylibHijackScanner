b38dc3f1ee2699bda639f4b89f0b9c8548a27c2c
its-a-feature <codybthomas@gmail.com>
2022-09-01 15:47:49 -0500

    Initial Commit

---
+++ dylibHijackScanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dylibHijackScanner
+//
+//  Created by Cody Thomas on 9/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
